d99c845605a95cff1b69d9ac141d818113d96253
Anton <shkarupinanton@yandex.ru>
2017-02-22 15:23:23 +0300

    Update radio.py

--- radio.py
+++ radio.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from grab import Grab
-import time
+from time import sleep
+import pytils
 
 def radio():
+
+    print pytils.dt.ru_strftime("%d %B %Y", inflected=True)
 
     url = 'http://radiovesti.ru/brand/60948/'
     g = Grab()
@@ -12,17 +15,18 @@ def radio():
         scr = 'http://radiovesti.ru'+str(elem.attr('href'))
         s = Grab()
         s.go(scr)
-        for element in s.doc.select("//a[@class='listen-news']"):
-            time.sleep(1)
-            scr_audio = ("http://audio.rutv.ru/download?id="+str(element.attr('data-audio-id')))[:-1]
-            f = Grab()
-            name = s.doc.select("//h1[@class='h1-title']").text()
-            name = name.replace(":", "")
-            name = name.replace("?", "")
-            name = name.replace("!", "")
-            print name
-            print scr_audio
-            f.go(scr_audio, log_file=name+'.mp3')
+        if pytils.dt.ru_strftime("%d %B %Y", inflected=True) in s.doc.select("//div[@class='news__date'][1]").text():
+            for element in s.doc.select("//a[@class='listen-news']"):
+                sleep(1)
+                scr_audio = ("http://audio.rutv.ru/download?id="+str(element.attr('data-audio-id')))[:-1]
+                f = Grab()
+                name = s.doc.select("//h1[@class='h1-title']").text()
+                name = name.replace(":", "")
+                name = name.replace("?", "")
+                name = name.replace("!", "")
+                print name
+                print scr_audio
+                f.go(scr_audio, log_file=name+'.mp3')
 
     print 'Конец'
 

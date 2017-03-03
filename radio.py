8a9e890ef362cc6c8e4f7b16a6549cf1293d51a5
Anton <shkarupinanton@yandex.ru>
2017-03-03 13:44:43 +0300

    Update radio.py

--- radio.py
+++ radio.py
@@ -4,18 +4,20 @@ from grab import Grab
 from time import sleep
 import pytils
 
+
 def radio():
 
-    print pytils.dt.ru_strftime("%d %B %Y", inflected=True)
-
+    print pytils.dt.ru_strftime(u'%d %B %Y', inflected=True)
     url = 'http://radiovesti.ru/brand/60948/'
     g = Grab()
     g.go(url)
+
     for elem in g.doc.select("//div[@class='news__content']//h1/a"):
         scr = 'http://radiovesti.ru'+str(elem.attr('href'))
         s = Grab()
         s.go(scr)
-        if pytils.dt.ru_strftime("%d %B %Y", inflected=True) in s.doc.select("//div[@class='news__date'][1]").text():
+        i = 0
+        if pytils.dt.ru_strftime(u"%d %B %Y", inflected=True) in s.doc.select("//div[@class='news__date'][1]").text():
             for element in s.doc.select("//a[@class='listen-news']"):
                 sleep(1)
                 scr_audio = ("http://audio.rutv.ru/download?id="+str(element.attr('data-audio-id')))[:-1]
@@ -25,9 +27,10 @@ def radio():
                 name = name.replace("?", "")
                 name = name.replace("!", "")
                 print name
-                print scr_audio
+                # print scr_audio
                 f.go(scr_audio, log_file=name+'.mp3')
+                i = + 1
 
-    print 'Конец'
+    print '\nКонец, загружено --->',i ,'файлов'
 
 radio()
